12f93e522e6f673c3b8e2025db30021c45819afb
sharifahmad <sharifahmad2061@gmail.com>
2021-04-19 11:09:43 +0500

    changed Task to TaskAdmin

--- Task/admin.py
+++ Task/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Task.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "created_on", "created_by")
+
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
