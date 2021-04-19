cae6b2e1e06b63d845c5cd254e9656d354befdd0
sharifahmad <sharifahmad2061@gmail.com>
2021-04-19 12:31:37 +0500

    added list task viewset

--- Task/views.py
+++ Task/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from rest_framework import request, status
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework import viewsets, permissions
 from rest_framework.generics import CreateAPIView
@@ -26,3 +28,34 @@ class TaskViewSet(viewsets.ModelViewSet):
         queryset = Task.objects.filter(created_by=request.user)
         serializer = TaskSerializer(queryset, many=True, context={"request": request})
         return Response(serializer.data)
+
+
+class ListTaskViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request):
+        queryset = Task.objects.filter(created_by=self.request.user)
+        serializer = TaskSerializer(queryset, many=True, context={"request": request})
+        return Response(serializer.data)
+
+
+class CreateTaskViewSet(viewsets.ViewSet):
+    http_method_names = ["POST"]
+    queryset = Task.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request):
+        try:
+            print(request.data)
+            task = Task(
+                content=request.data.get("content"), created_by=self.request.user
+            )
+            print("1", task)
+            task = Task.objects.create(task)
+            print("2", task)
+            serializer = TaskSerializer(task, context={"request": request})
+            return Response(serializer.data)
+        except Exception as exc:
+            return Response(
+                {"message": exc.__str__()}, status=status.HTTP_400_BAD_REQUEST
+            )
