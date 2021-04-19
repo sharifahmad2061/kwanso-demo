9e59ad061f93a83e7b1d786fb30c89b132b84000
sharifahmad <sharifahmad2061@gmail.com>
2021-04-19 22:57:59 +0500

    removed unnecessary imports

--- Task/views.py
+++ Task/views.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import User
-from django.db.models import query
 from rest_framework import status
 from rest_framework import viewsets, permissions
-from rest_framework import request
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from Task.models import Task
 from Task.serializers import UserSerializer, TaskSerializer
