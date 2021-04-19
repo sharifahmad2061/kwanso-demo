33b415acf5bdc31f4b131e348dd354a25edec6d9
sharifahmad <sharifahmad2061@gmail.com>
2021-04-19 20:35:55 +0500

    corrected the errors, need to incorporate auth

--- Task/views.py
+++ Task/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models import query
 from rest_framework import status
 from rest_framework import viewsets, permissions
+from rest_framework import request
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from Task.models import Task
@@ -11,8 +12,23 @@ from Task.serializers import UserSerializer, TaskSerializer
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes_by_action = {
+        "create": [permissions.AllowAny],
+        "list": [permissions.IsAuthenticated],
+        "destroy": [permissions.IsAdminUser],
+    }
+
+    def list(self, request):
+        try:
+            serializer = UserSerializer(self.request.user)
+            return Response(serializer.data)
+        except Exception as exc:
+            return Response(
+                {"message": exc.__str__()}, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -18,10 +18,6 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView
 from Task.views import (
-    CreateUserViewSet,
-    RetrieveUserViewSet,
-    CreateTaskViewSet,
-    ListTaskViewSet,
     UsersViewSet,
     TaskViewSet,
 )
