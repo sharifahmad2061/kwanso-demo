71ba54856320b14c9256c34b6419a1264da0d9b0
sharifahmad <sharifahmad2061@gmail.com>
2021-04-19 11:09:15 +0500

    changed user serializer

--- Task/serializers.py
+++ Task/serializers.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "username", "email", "groups"]
+        fields = ["first_name", "last_name", "username", "email", "password", "url"]
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
